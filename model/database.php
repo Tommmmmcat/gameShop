6539b6eb7a664ae8e464c759a584932f246dea0d
Tommmmmcat <61011490+Tommmmmcat@users.noreply.github.com>
2021-01-14 20:43:57 +0000

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,25 +1,22 @@
 <?php
 
-$dbhost = $_SERVER['mycadb.cm01n5xj8z88.eu-west-1.rds.amazonaws.com'];
-$dbport = $_SERVER['3306'];
-$dbname = $_SERVER['gameshop'];
+$dbhost = "mycadb.cm01n5xj8z88.eu-west-1.rds.amazonaws.com";
+$dbport = "3306";
+$dbname = "gameshop";
 $charset = 'utf8' ;
 
 
-
 $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-//$dsn = "mysql:host=localhost;dbname=gameshop";
 $username = "mysqldbuser";
 $password = "mysqldbpassword";
 
 
 try {
     $db = new PDO($dsn, $username, $password);
-    //set up error reporting on server
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $ex) {
-    echo "Connection Failure Error is " . $ex->getMessage();
-
-
+} catch (Exception $ex) {
+    $error_message = $ex->getMessage();
+    echo $error_message;
+    exit();
 }
